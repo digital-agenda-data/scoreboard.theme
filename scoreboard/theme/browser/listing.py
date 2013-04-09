8d0bfefb63b32684927833a379e4c6cdbd7f0edb
David Batranu <dbatranu@gmail.com>
2013-04-09 15:42:08 +0300

    Updating homepage listing
    - datacubes listed on homepage
    - visualizations listed on cube/@@visualizations
    - updating styles
    - adding default image for datacube

--- scoreboard/theme/browser/listing.py
+++ scoreboard/theme/browser/listing.py
@@ -2,10 +2,13 @@ from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
 
 
-class VisualizationsListingView(BrowserView):
+class HomepageListingView(BrowserView):
 
     def getDataCubes(self):
         query = {'portal_type': 'DataCube'}
         catalog = getToolByName(self.context, 'portal_catalog');
         return [b.getObject() for b in catalog(**query)]
 
+    def getItemState(self, obj):
+        wft = getToolByName(self.context, 'portal_workflow')
+        return wft.getInfoFor(obj, 'review_state', '')
