527ac056f5c70c932104ac6dfec08128c26ba313
Alin Voinea <alin@eaudeweb.ro>
2013-04-11 19:39:24 +0300

    Refs #1101 - Reorder datacubes via drag-and-drop within listing view

--- scoreboard/theme/browser/js/listing.js
+++ scoreboard/theme/browser/js/listing.js
@@ -1,22 +1,98 @@
-if(window.scoreboard === undefined){
-    var scoreboard = {
-        version: '1.0'
+if(window.Scoreboard === undefined){
+  var Scoreboard = {
+    version: '1.0'
+  };
+}
+
+Scoreboard.Events = {
+  sorted: 'scoreboard-sorted'
+};
+
+Scoreboard.Listing = function(context, options){
+  var self = this;
+  self.context = context;
+
+  self.settings = {
+    items: '.scoreboard-datacubes-listing-item',
+    action: 'folder_moveitem'
+  };
+
+  if(options){
+    jQuery.extend(self.settings, options);
+  }
+
+  self.initialize();
+};
+
+Scoreboard.Listing.prototype = {
+  initialize: function(){
+    var self = this;
+
+    var items = jQuery(self.settings.items, self.context);
+    var parent = items.data('parent') ? items.data('parent') + '/' : '';
+    self.settings.action = parent + self.settings.action;
+
+    items.attr('title', 'Click and drag to reorder');
+    items.each(function(idx, item){
+      jQuery(item).data('order', idx);
+    });
+
+    self.context.sortable({
+      items: self.settings.items,
+      placeholder: 'ui-state-highlight',
+      forcePlaceholderSize: true,
+      opacity: 0.7,
+      delay: 300,
+      cursor: 'crosshair',
+      tolerance: 'pointer',
+      update: function(event, ui){
+        self.sort(ui.item, parent);
+      }
+    });
+  },
+
+  sort: function(context, parent){
+    var self = this;
+
+    var delta = context.index() - context.data('order');
+
+    var query = {
+      delta: delta,
+      item_id: context.data('name')
     };
+
+    $.ajax({
+        type: 'POST',
+        url: self.settings.action,
+        data: query,
+        traditional: true,
+        complete: function(){
+          jQuery(document).trigger(Scoreboard.Events.sorted);
+        }
+    });
+  }
 };
 
-if(scoreboard.theme === undefined){
-    scoreboard.theme = {};
+jQuery.fn.ScoreboardListing = function(options){
+  return this.each(function(){
+    var context = jQuery(this);
+    var adapter = new Scoreboard.Listing(context, options);
+    context.data('ScoreboardListing', adapter);
+  });
 };
 
-scoreboard.theme.listing = {
-    makeTabs: function(){
-        jQuery("ul.tabs").tabs("div.panes > div", {
-            'tabs': 'li',
-            'current': 'active'
-        });
-    }
-};
-
+// On document ready
 jQuery(document).ready(function(){
-    scoreboard.theme.listing.makeTabs();
+
+  // Enhance datacubes listing
+  var items = jQuery('.scoreboard-datacubes-listing');
+  if(items.length){
+    items.ScoreboardListing({
+      items: '.scoreboard-datacubes-listing-item',
+      action: 'folder_moveitem'
+    });
+  }
+
+  // Enhance visualizations listing
+
 });
--- scoreboard/theme/browser/listing.py
+++ scoreboard/theme/browser/listing.py
@@ -1,14 +1,37 @@
+""" Listing Views
+"""
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
 
 
 class HomepageListingView(BrowserView):
-
+    """ Listing for homepage
+    """
     def getDataCubes(self):
-        query = {'portal_type': 'DataCube'}
+        """ All Data Cubes sorted by their position in parent
+        """
+        query = {
+            'portal_type': 'DataCube',
+            'sort_on': 'getObjPositionInParent'
+        }
+
         catalog = getToolByName(self.context, 'portal_catalog');
         return [b.getObject() for b in catalog(**query)]
 
     def getItemState(self, obj):
+        """ Item state
+        """
         wft = getToolByName(self.context, 'portal_workflow')
         return wft.getInfoFor(obj, 'review_state', '')
+
+
+class VisualizationsListingView(BrowserView):
+    """ Visualizations listing
+    """
+    def visualizations(self):
+        """
+        Return all visualization for this context sorted by position
+        in parent
+        """
+        relations = self.context.getBRefs()
+        return relations
