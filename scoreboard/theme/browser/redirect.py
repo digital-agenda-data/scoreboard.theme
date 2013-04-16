98966e00469d13eb30eb15b552f0593b4849fec4
David Batranu <dbatranu@gmail.com>
2013-04-16 14:51:24 +0300

    Redirect dataset and visualizations containers pages to portal index

---
+++ scoreboard/theme/browser/redirect.py
@@ -0,0 +1,14 @@
+""" Redirect Views
+"""
+from zope.app.component.hooks import getSite
+from Products.Five.browser import BrowserView
+
+
+class DatasetContainerRedirectView(BrowserView):
+    def __call__(self, *args, **kwargs):
+        return self.request.response.redirect(getSite().absolute_url())
+
+class VisualizationsContainerRedirectView(BrowserView):
+    def __call__(self, *args, **kwargs):
+        return self.request.response.redirect(getSite().absolute_url())
+
