304fcba1d5a759b5eba29a198f3247be8e275b52
Andrei Melis <andrei.melis@eaudeweb.ro>
2015-10-16 10:34:33 +0300

    Piwik site id in custom javascript

--- scoreboard/theme/browser/js/piwik.js
+++ scoreboard/theme/browser/js/piwik.js
@@ -1,4 +1,6 @@
 var _paq = _paq || [];
+// siteId: 1=test.digital-agenda-data.eu, 2=digital-agenda-data.eu
+var _piwikSiteId = _piwikSiteId || 1;
 // Check for cookie consent
 if (readCookie("_accept_cookies") === "false") {
   _paq.push(['disableCookies']);
@@ -8,7 +10,7 @@ _paq.push(['enableLinkTracking']);
 (function() {
   var u=(("https:" == document.location.protocol) ? "https" : "http") + "://test.digital-agenda-data.eu/analytics/piwik/";
   _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', 1]);
+  _paq.push(['setSiteId', _piwikSiteId]);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
   g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
