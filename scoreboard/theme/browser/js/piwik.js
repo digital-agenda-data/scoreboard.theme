b392e04decee00d371204488c34b0a7b760a7fa9
Andrei Melis <andrei.melis@eaudeweb.ro>
2016-10-13 19:48:02 +0300

    Track site search in piwik

--- scoreboard/theme/browser/js/piwik.js
+++ scoreboard/theme/browser/js/piwik.js
@@ -1,16 +1,26 @@
-var _paq = _paq || [];
-// siteId: 1=test.digital-agenda-data.eu, 2=digital-agenda-data.eu
-var _piwikSiteId = _piwikSiteId || 1;
-// Check for cookie consent
-if (readCookie("_accept_cookies") === "false") {
-  _paq.push(['disableCookies']);
+pwk = function(event) {
+    var _paq = _paq || [];
+    // siteId: 1=test.digital-agenda-data.eu, 2=digital-agenda-data.eu
+    var _piwikSiteId = _piwikSiteId || 1;
+    // Check for cookie consent
+    if (readCookie("_accept_cookies") === "false") {
+      _paq.push(['disableCookies']);
+    }
+    var searchCount = document.querySelector(".docsTable").getAttribute('data-search-result-count');
+    _paq.push(['setCustomUrl', document.URL + '&search_count=' + searchCount]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u=(("https:" == document.location.protocol) ? "https" : "http") + "://test.digital-agenda-data.eu/analytics/piwik/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', _piwikSiteId]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+      g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+};
+if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', pwk);
+} else if (document.attachEvent) {
+    // IE8
+    window.onload = pwk;
 }
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u=(("https:" == document.location.protocol) ? "https" : "http") + "://test.digital-agenda-data.eu/analytics/piwik/";
-  _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', _piwikSiteId]);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
-  g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-})();
